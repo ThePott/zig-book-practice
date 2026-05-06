7549a382497231358f2170a12c9baca1a93a3449
Tynahan <nusilite@gmail.com>
2026-05-06 20:11:11 +0900

    feat: replace lock with atomic value

--- src/main.zig
+++ src/main.zig
@@ -2,43 +2,31 @@ const std = @import("std");
 const expect = std.testing.expect;
 const Allocator = std.mem.Allocator;
 
-const duration = std.Io.Duration.fromSeconds(1);
-const clock = std.Io.Clock.awake;
-fn sleep(io: std.Io) !void {
-    try std.Io.sleep(io, duration, clock);
-}
+var count = std.atomic.Value(usize).init(0);
 
-var count: usize = 0;
 fn print() void {
-    std.debug.print("count: {any}\n", .{count});
+    const current = count.load(.seq_cst);
+    std.debug.print("count: {any}\n", .{current});
 }
 
-fn printInLoop(io: std.Io, read_write_lock: *std.Io.RwLock) !void {
+fn printInLoop() !void {
     while (true) {
-        try read_write_lock.lockShared(io);
         print();
-        read_write_lock.unlockShared(io);
-        // try sleep(io);
     }
 }
 
-fn incrementInLoop(io: std.Io, read_write_lock: *std.Io.RwLock) !void {
-    for (0..1000000) |_| {
-        try read_write_lock.lock(io);
-        count += 1;
-        read_write_lock.unlock(io);
-        // try sleep(io);
+fn incrementInLoop() !void {
+    for (0..100000) |_| {
+        _ = count.fetchAdd(1, .seq_cst);
     }
 }
 
-pub fn main(init: std.process.Init) !void {
-    const io = init.io;
-    var read_write_lock = std.Io.RwLock.init;
-    const thread_1 = try std.Thread.spawn(.{}, printInLoop, .{ io, &read_write_lock });
-    const thread_2 = try std.Thread.spawn(.{}, printInLoop, .{ io, &read_write_lock });
-    const thread_3 = try std.Thread.spawn(.{}, printInLoop, .{ io, &read_write_lock });
-    const thread_4 = try std.Thread.spawn(.{}, incrementInLoop, .{ io, &read_write_lock });
-    const thread_5 = try std.Thread.spawn(.{}, incrementInLoop, .{ io, &read_write_lock });
+pub fn main() !void {
+    const thread_1 = try std.Thread.spawn(.{}, printInLoop, .{});
+    const thread_2 = try std.Thread.spawn(.{}, printInLoop, .{});
+    const thread_3 = try std.Thread.spawn(.{}, printInLoop, .{});
+    const thread_4 = try std.Thread.spawn(.{}, incrementInLoop, .{});
+    const thread_5 = try std.Thread.spawn(.{}, incrementInLoop, .{});
     thread_1.detach();
     thread_2.detach();
     thread_3.detach();
